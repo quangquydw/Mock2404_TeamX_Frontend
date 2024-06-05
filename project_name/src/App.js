50f1ad46e98bae8f39e3a4ef7594eb333734a3a7
quangquydw <quangquydhv@gmail.com>
2024-06-05 20:24:25 +0700

    tao project frontend va day len git hub

---
+++ project_name/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+           project_name
+      </h1>
+    </div>
+  );
+}
+
+export default App;
